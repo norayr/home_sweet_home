816e9c3e02f7fef39547f47035e5bb2b69d0bf85
norayr <norayr@arnet.am>
2017-03-10 19:43:37 +0400

    nth

--- scripts/gif/test.sh
+++ scripts/gif/test.sh
@@ -1,10 +1,12 @@
 source ./make_animation.sh
 
-list=`gen_list _MG_ JPG 8452 8472`
+#list=`gen_list _MG_ JPG 8452 8472`
+list=`gen_list _MG_ JPG 8452 8500`
 
 
 echo $list
 newlist=`quarter_size small $list`
 newlist2=`quarter_size smaller $newlist`
 
-make_animation test2 15 $newlist2
+#make_animation test2 15 $newlist2
+make_animation test3 15 $newlist2
