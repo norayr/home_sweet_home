107c838a5fe8fbe60e0a03e1046fc42bcdcfe3f1
Norayr Chilingarian <norayr@arnet.am>
2017-04-09 01:01:58 +0400

    Revert "moved gif maker to  separate repository; -- noch"
    This reverts commit 3d4a615c2f6856be6fdce9aa4b4df4eab36a767b.

---
+++ scripts/make_gif_animation/make_animation.sh
@@ -0,0 +1,244 @@
+#first argument - name of the output gif
+#second argument - delay
+#next arguments - list of files to make animation from them.
+set -x
+function make_animation()
+{
+  list=""
+
+  for i in $@
+  do
+
+    if [[ "$i" == "$1" ]]
+	then
+      outname=$i
+	elif [[ "$i" == "$2" ]]
+	then
+      delay=$i
+	else
+      list="${list} ${i}"
+	fi
+
+  done
+
+  convert -loop 0 -delay $delay $list $outname.gif
+
+}
+
+function make_strob_animation()
+{
+  list=""
+  j=0
+  for i in $@
+  do
+
+    if [[ "$i" == "$1" ]]
+	then
+      outname=$i
+	elif [[ "$i" == "$2" ]]
+	then
+      delay=$i
+	else
+      # this way we add only every second image to the list.
+      let "j++"
+
+      if [ $((j%2)) -eq 0 ];
+      then
+        #echo "even";
+        list="${list} ${i}"
+      #else
+        #echo "odd";
+      fi
+   	fi
+
+  done
+
+  convert -loop 0 -delay $delay $list $outname.gif
+
+}
+
+  #first argument - prefix, like "shot"
+  #second argument - file extension, like "png"
+  #third argument - first number
+  #fourth argument - last number
+  function gen_list()
+  {
+  list=""
+  for i in `seq $3 $4`
+  do
+	#newnumber='00000'${i#foo}      # get number, pack with zeros
+	#newnumber=${newnumber:(-5)}       # the last five characters
+    #number=`printf ${1}%04d.${2} $i` #mplayer shots look like shot0042.png
+    number=`printf %04d $i`
+	item="${1}${number}.${2}"
+	if [ -f $item ]; then
+      echo $item
+	fi
+	#list="$list $item"
+
+  done
+  #$5=$list 
+  }
+
+  # creates files with half size of the originals and returns the file list.
+  # arguments:
+  # $1 - postfix, like "half" or "small" or "sm"
+  # $2 - input list
+  # caller is encouraged to remove temporary files after using them.
+
+  function half_size()
+  {
+  #creating input list and finding postfix
+  input=""
+  for i in $@
+  do
+
+    if [[ "$i" == "$1" ]]
+    then
+      postfix=$i 
+    else
+      input="${input} ${i}"
+    fi
+
+  done
+
+  output=""
+
+  for i in $input
+  do
+    
+	#filename=$(basename "$fullfile")
+	extension="${i##*.}"
+	filename="${i%.*}"
+    newfilename="${filename}_${postfix}.${extension}"
+    convert -resize %50 $i ${newfilename}
+    output="${output} ${newfilename}"
+    echo ${newfilename}
+  done
+
+    $1=$output
+
+  }
+
+  function quarter_size()
+  {
+  #creating input list and finding postfix
+  input=""
+  for i in $@
+  do
+
+    if [[ "$i" == "$1" ]]
+    then
+      postfix=$i 
+    else
+      input="${input} ${i}"
+    fi
+
+  done
+
+  output=""
+
+  for i in $input
+  do
+    
+	#filename=$(basename "$fullfile")
+	extension="${i##*.}"
+	filename="${i%.*}"
+    newfilename="${filename}_${postfix}.${extension}"
+    convert -resize %25 $i ${newfilename}
+    output="${output} ${newfilename}"
+    echo ${newfilename}
+  done
+
+    $1=$output
+
+  }
+
+  function eighth_size()
+  {
+  #creating input list and finding postfix
+  input=""
+  for i in $@
+  do
+
+    if [[ "$i" == "$1" ]]
+    then
+      postfix=$i 
+    else
+      input="${input} ${i}"
+    fi
+
+  done
+
+  output=""
+
+  for i in $input
+  do
+    
+	#filename=$(basename "$fullfile")
+	extension="${i##*.}"
+	filename="${i%.*}"
+    newfilename="${filename}_${postfix}.${extension}"
+    convert -resize %12.5 $i ${newfilename}
+    output="${output} ${newfilename}"
+    echo ${newfilename}
+  done
+
+    $1=$output
+
+  }
+
+  function sixteenth_size()
+  {
+  #creating input list and finding postfix
+  input=""
+  for i in $@
+  do
+
+    if [[ "$i" == "$1" ]]
+    then
+      postfix=$i 
+    else
+      input="${input} ${i}"
+    fi
+
+  done
+
+  output=""
+
+  for i in $input
+  do
+    
+	#filename=$(basename "$fullfile")
+	extension="${i##*.}"
+	filename="${i%.*}"
+    newfilename="${filename}_${postfix}.${extension}"
+    convert -resize %6.25 $i ${newfilename}
+    output="${output} ${newfilename}"
+    echo ${newfilename}
+  done
+
+    $1=$output
+
+  }
+
+# assumes that prefix is 'shot', postfix - '_small', extension - 'png'.
+# arguments:
+# $1 - output file name
+# $2 - initial number of screenshot
+# $3 - last number of screenshot
+# $4 - delay
+
+function animate_mplayer_screenshots()
+{
+start_frame=$2
+end_frame=$3
+lst=`gen_list shot png $start_frame $end_frame`
+newlst=`half_size small $lst`
+make_animation $1 $4 $lst
+make_strob_animation ${1}_ $4 $lst
+make_animation ${1}_small $4 $newlst
+make_strob_animation ${1}_small_ $4 $newlst
+rm -f $newlst
+}
+
---
+++ scripts/make_gif_animation/readme.md
@@ -0,0 +1,136 @@
+functions to make animations from file lists, or generate those lists.
+
+using
+====
+
+to use it, "import" it.
+
+```
+source ./make_animation.sh
+```
+
+functions
+=========
+
+* make_animation()
+
+arguments:
+
+** name of the output gif file.
+
+** delay
+
+** list of files
+
+usage:
+```
+make_animation rain 16 $list
+```
+
+* make_strob_animation()
+
+the function differs from the previous one in that it only uses every second file from the list.
+
+
+* gen_list()
+
+this function is useful to make animations by only mentioning mplayer screenshot numbers.
+
+let's say we want to make an animation from the files shot0023.png .. shot0042.png. for that we call gen_list() by giving it two numbers, i. e. 23 and 42 and get the list as the return value.
+
+arguments:
+
+** prefix (i. e. "shot")
+
+** extension (i. e. "png")
+
+** first number
+
+** last number
+
+** list to return
+
+usage:
+```
+list=`gen_list shot png 23 42`
+```
+
+* half_size()
+
+arguments:
+
+** $1 - some postfix, like "_small". if the input files look like "shot007.png" then output files will look like "shot007_small.png".
+
+** $2 - input list.
+
+usage:
+
+```
+newlist=`half_size small $list`
+```
+
+
+* quarter_size()
+
+the same as the function above, but will create even smaller files.
+
+* animate_mplayer_screenshots()
+
+this function will take the name of the output file and initial and last numbers of frames, and delay:
+
+** $1 - output file name without extension
+** $2 - initial number of frame
+** $3 - last number of frame
+** $4 - delay value
+
+usage:
+
+```
+animate_mplayer_screenshots rain 23 42 16
+```
+
+examples
+=======
+
+to get list of files:
+
+```
+gen_list shot png 23 42 $list
+```
+to make gif from them:
+
+```
+make_animation rain 15 $list
+```
+
+will create rain.gif from the $list list.
+
+```
+make_strob_animation rain 15 $list
+```
+
+will create smaller output animation because it'll take only every second file.
+
+it's possible to resize files before making an animation.
+
+```
+list=`gen_list shot png 70 85`
+newlist=`half_size small $list`
+make_animation gif/drawing 16 $list
+make_strob_animation gif/drawing_ 16 $list
+make_animation gif/drawing_small 16 $newlist
+make_strob_animation gif/drawing_small_ 16 $newlist
+rm -f $newlist
+```
+
+caller is responsible in removing temporary resized files.
+
+or the same
+
+```
+animate_mplayer_screenshots gif/drawing 70 85 16
+```
+
+p. s.
+in order to make screenshots, I use mplayer with ```-vf screenshot``` argument, then press 's' to make a screenshot. (:
+
