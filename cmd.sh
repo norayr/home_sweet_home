0b37474b8a9f7ec1927ed07a0115c687ccca46ec
norayr <norayr@arnet.am>
2017-08-30 02:11:12 +0400

    darkside

--- cmd.sh
+++ cmd.sh
@@ -1,1 +1,1 @@
-/usr/bin/mpg123 '/mnt/floppy/music/nicolas_jaar/Nicolas Jaar Boiler Room NYC DJ Set at Clown & Sunset Takeover-IUjWumGIqe8.mp3'
+/usr/bin/mplayer '/mnt/floppy/music/nicolas_jaar/DARKSIDE Boiler Room NYC Live Set-g3AMQCf4lj4.ogg'
