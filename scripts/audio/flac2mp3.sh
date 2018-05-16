5532b59791d7f6caf35dbd50d78306a4393b9341
norayr <norayr@arnet.am>
2018-05-17 02:50:51 +0400

    flac2mp3 fix

--- scripts/audio/flac2mp3.sh
+++ scripts/audio/flac2mp3.sh
@@ -1,6 +1,9 @@
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
 for i in *.flac
 do
 nm=${i%.*}
-#echo $nm
-ffmpeg -i $i -ab 320k -map_metadata 0 -id3v2_version 3 ${nm}.mp3
+echo $nm
+ffmpeg -i "$i" -ab 320k -map_metadata 0 -id3v2_version 3 ${nm}.mp3
 done
+IFS=$SAVEIFS
