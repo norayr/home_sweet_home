67eca956a763a79c85e148aa4196524ac04c5e00
norayr <norayr@arnet.am>
2018-06-04 19:05:32 +0400

    clicks

---
+++ scripts/clicks.sh
@@ -0,0 +1,8 @@
+#this can be used in order to prevent remote citrix windows to log off
+while true
+do
+xdotool mousemove 300 150 click 1
+sleep 30
+xdotool mousemove 320 150 click 1
+sleep 30
+done
