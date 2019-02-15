3e47779ac06af6761f49795c83f99b97ae00bad8
norayr <norayr@arnet.am>
2019-02-15 15:24:27 +0400

    added lock to aplaywrapper.sh

--- bin/aplaywrapper.sh
+++ bin/aplaywrapper.sh
@@ -1,4 +1,12 @@
-vol=`amixer get Master | grep % | awk {'print $4'} | awk '{ print substr( $0, 2, 2 ) }'`
-amixer set Master 30%
-aplay $1
-amixer set Master $vol
+lock=/tmp/aplaywrapper.tmp
+
+if [ -f $lock ]; then
+   echo "File $FILE exists."
+else
+  touch $lock
+  vol=`amixer get Master | grep % | awk {'print $4'} | awk '{ print substr( $0, 2, 2 ) }'`
+  amixer set Master 50%
+  aplay $1
+  amixer set Master $vol
+  rm $lock
+fi
