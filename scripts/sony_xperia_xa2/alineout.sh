61dc801f0db185e2b011764fe42e582b109ee80f
norayr <norayr@arnet.am>
2019-03-13 15:57:27 +0400

    xperia xa2 sailfish route audio

---
+++ scripts/sony_xperia_xa2/alineout.sh
@@ -0,0 +1,7 @@
+pacmd list-sinks
+#pacmd set-sink-port sink.primary_output output-wired_headphone
+#pacmd set-sink-port sink.primary_output output-speaker
+pacmd set-sink-port sink.primary_output output-line
+#pacmd set-sink-port sink.primary_output output-earpiece
+#pacmd set-sink-port sink.primary_output output-sco_headset
+#pacmd set-sink-port sink.primary_output output-sco_carkit
