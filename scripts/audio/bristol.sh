75a767d8f5eeddc0237660cafb8856d867d0b815
norayr chilingarian <norayr@arnet.am>
2019-05-30 23:53:37 +0400

    bristol

--- scripts/audio/bristol.sh
+++ scripts/audio/bristol.sh
@@ -1,1 +1,5 @@
-startBristol -mini -audio alsa -audiodev plughw:1,0
+#https://spyurk.am/posts/3226265
+#startBristol -mini -audio alsa -audiodev plughw:2,0
+startBristol  -prophet -audio alsa -audiodev plughw:2,0 -midi alsa -channel 1 -mididev /dev/midi1 &
+sleep 3
+aconnect 20:0 128:0
