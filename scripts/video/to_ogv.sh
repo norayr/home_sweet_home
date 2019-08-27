6faa9043e0828d52d72ff606e09b578a5db02d9b
Norayr Chilingarian <norayr@arnet.am>
2019-08-27 15:30:07 +0400

    theora

--- scripts/video/to_ogv.sh
+++ scripts/video/to_ogv.sh
@@ -1,1 +1,2 @@
-ffmpeg -i $1 -acodec libvorbis -vcodec libtheora -ac 2 -ab 96k -ar 44100 -b 819200 output.ogv
+#ffmpeg -i $1 -acodec libvorbis -vcodec libtheora -ac 2 -ab 96k -ar 44100 -b 819200 output.ogv
+ffmpeg -i $1 -c:v libtheora -q:v 7 -c:a libvorbis -q:a 4 output.ogv
