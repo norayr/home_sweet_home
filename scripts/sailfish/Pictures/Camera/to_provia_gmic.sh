84594680fa5919f7fecb5c165bad578a51b15e08
Norayr Chilingarian <norayr@arnet.am>
2020-02-01 16:18:21 +0400

    chosen films

--- scripts/sailfish/Pictures/Camera/to_provia_gmic.sh
+++ scripts/sailfish/Pictures/Camera/to_provia_gmic.sh
@@ -2,6 +2,7 @@ set -x
 i=$1
 
 j="${i%.*}"
-#darktable-cli $i fujichrome_provia_100f.xmp ${j}_provia.jpg
-gmic -input $i map_clut fuji_provia_100f -output ${j}_provia100f_gmic.jpg
+darktable-cli $i fujichrome_provia_100f.xmp ${j}_provia.jpg
+gmic -input $i map_clut fuji_provia_400f -output ${j}_provia400f_gmic.jpg
+gmic -input $i map_clut kodak_elite_color_400 -output ${j}_kec400_gmic.jpg
 
