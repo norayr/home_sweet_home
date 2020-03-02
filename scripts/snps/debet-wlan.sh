48e9d07058a406a4cc5741ea1eff02e502d69cc5
Norayr Chilingarian <norayr@arnet.am>
2020-03-02 17:52:04 +0400

    added jabberam ip

--- scripts/snps/debet-wlan.sh
+++ scripts/snps/debet-wlan.sh
@@ -13,7 +13,8 @@ sleep 3
 #ip route add default via ${IP0}1 dev wlan0 table 502
     gw="${IP0}1"
     arnet="81.16.1.26"
-	protonvpn="35.160.222.63"
+    jabberam="37.252.73.34"
+	  protonvpn="35.160.222.63"
     freenode="130.239.18.119 38.229.70.22 130.185.232.126 193.10.255.100 164.132.77.237 94.125.182.252 82.96.96.11 162.213.39.42 64.86.243.181 162.213.39.42 185.30.166.38 185.30.166.37 94.125.182.252 204.225.96.251 149.56.134.238 195.154.200.232 107.182.226.199"
     pine64="212.227.94.216"
     photonet="54.236.123.137 208.69.33.154 146.112.196.77"
@@ -21,7 +22,7 @@ sleep 3
     xmppjp="160.16.217.191"
     funtoo="192.150.253.217"
     github="192.30.253.112 192.30.253.113 140.82.113.3 140.82.113.4 140.82.114.3 140.82.114.4"
-    hosts="$arnet $protonvpn $freenode $pine64 $photonet $jabberorg $xmppjp $funtoo $github"
+    hosts="$arnet $jabberam $protonvpn $freenode $pine64 $photonet $jabberorg $xmppjp $funtoo $github"
     for hst in $hosts
     do
       #sudo route del -host $hst dev wlan0
