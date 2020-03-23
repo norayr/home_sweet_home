25bd7988df187364f23fc58b8ff3126fbc693063
Norayr Chilingarian <norayr@fqdn>
2020-03-23 21:21:07 +0400

    ufraw

--- scripts/photo/ufraw.sh
+++ scripts/photo/ufraw.sh
@@ -1,6 +1,7 @@
 for i in *.dng
 do
   j="${i%.*}"
-  ufraw-batch --out-type=jpg --compression=100 --out-depth=16 --clip=film --output=${j}_ufraw.jpg $i
+  ufraw-batch --wb=auto --out-type=tiff --out-depth=16 --clip=film --output=${j}_ufraw.tiff $i
+  ufraw-batch --wb=auto --out-type=jpg --compression=93 --out-depth=16 --clip=film --output=${j}_ufraw.jpg $i
 done
 
