280ca6b5a9dc2b8dc8f705a689e5f1c105afaf24
Norayr Chilingarian <norayr@arnet.am>
2020-05-03 19:12:37 +0400

    added link

--- scripts/video/mplayer-youtube-dl.sh
+++ scripts/video/mplayer-youtube-dl.sh
@@ -1,1 +1,2 @@
+#found at https://www.yonamine.dev/2012/10/how-to-use-youtube-dl-mplayer-to-stream.html
 mplayer -ontop -cookies -cookies-file ./cookie.txt $(youtube-dl -gf 34 --cookies ./cookie.txt "https://www.youtube.com/watch?v=<VIDEO ID>")
