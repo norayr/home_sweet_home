b03e3f06f71cebc1011836b4101d489119968d17
Norayr Chilingarian <norayr@arnet.am>
2020-05-03 19:14:12 +0400

    mplayer youtube-dl update

--- scripts/video/mplayer-youtube-dl.sh
+++ scripts/video/mplayer-youtube-dl.sh
@@ -1,2 +1,5 @@
 #found at https://www.yonamine.dev/2012/10/how-to-use-youtube-dl-mplayer-to-stream.html
-mplayer -ontop -cookies -cookies-file ./cookie.txt $(youtube-dl -gf 34 --cookies ./cookie.txt "https://www.youtube.com/watch?v=<VIDEO ID>")
+#https://www.yonamine.dev/2014/09/how-to-use-curl-youtube-dl-mplayer-to.html
+#mplayer -ontop -cookies -cookies-file ./cookie.txt $(youtube-dl -gf 34 --cookies ./cookie.txt "https://www.youtube.com/watch?v=<VIDEO ID>")
+#youtube-dl -o - | mplayer -
+curl --ciphers RC4-SHA "$(youtube-dl -g https://www.youtube.com/watch?v=<VIDEO ID>)" | mplayer -
