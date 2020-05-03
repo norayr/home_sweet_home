f1ac339bffba3068ade75d522737a55831956ef3
Norayr Chilingarian <norayr@arnet.am>
2020-05-03 19:43:54 +0400

    canon dev video file

---
+++ scripts/video/canon_6d.sh
@@ -0,0 +1,6 @@
+#https://github.com/umlaeute/v4l2loopback
+#modprobe v4l2loopback exclusive_caps=1 max_buffers=2
+#create v4l device
+#gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video4
+#now you can play it:
+mplayer -tv device=/dev/video4 tv://
--- scripts/video/mplayer-tv.sh
+++ scripts/video/mplayer-tv.sh
@@ -1,1 +1,2 @@
-mplayer tv:// -tv driver=v4l2:width=640:height=480:device=/dev/video0 -fps 30
+#mplayer tv:// -tv driver=v4l2:width=640:height=480:device=/dev/video0 -fps 30
+mplayer -tv device=/dev/video0 tv://
