3185f10f65563d92957cab6b1263e0843f48387d
Norayr Chilingarian <norayr@arnet.am>
2020-08-25 21:33:51 +0400

    user memory

---
+++ scripts/mem/usermem.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+stats=””
+echo "%   user"
+echo "============"
+
+# collect the data
+for user in `ps aux | grep -v COMMAND | awk '{print $1}' | sort -u`
+do
+  stats="$stats\n`ps aux | egrep ^$user | awk 'BEGIN{total=0}; \
+    {total += $4};END{print total,$1}'`"
+done
+
+# sort data numerically (largest first)
+echo -e $stats | grep -v ^$ | sort -rn | head
+
