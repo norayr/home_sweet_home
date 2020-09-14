f5d6a9389205cfb043017c7523405e6cf22d56d9
Norayr Chilingarian <norayr@arnet.am>
2020-09-14 17:56:22 +0400

    ffmpeg fixes

--- scripts/video/ffmpeg_lut.sh
+++ scripts/video/ffmpeg_lut.sh
@@ -1,4 +1,5 @@
 LUT="Fujifilm_Eterna_Vivid_160T_(8543)_-_Look.cube"
+CODEC="-codec:v libx264 -codec:a copy"
 FN="${1%.*}"
-ffmpeg -i $1  -vf lut3d="$LUT"  ${FN}_eterna.mp4
+ffmpeg -i $1  -vf lut3d="$LUT" $CODEC ${FN}_eterna.mp4
 
