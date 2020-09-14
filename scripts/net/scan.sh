501cc84a3bcaeec1e5834f52fdbc8f8daebe0665
Norayr Chilingarian <norayr@arnet.am>
2020-09-14 19:40:12 +0400

    customized network environment

--- scripts/net/scan.sh
+++ scripts/net/scan.sh
@@ -1,6 +1,7 @@
+NET=2.2.2
 for i in {1..254}
 do 
-  (ping 192.168.1.$i -c 1 -w 5  >/dev/null && echo "192.168.1.$i" &) ;
+  (ping ${NET}.$i -c 1 -w 5  >/dev/null && echo "${NET}.$i" &) ;
 done
 
 #nmap -sP 192.168.1.1-254
