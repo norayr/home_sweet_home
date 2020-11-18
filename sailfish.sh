7979f3b4d9fbc9108653820bc8f97fd6c0d1fbc6
Norayr Chilingarian <norayr@arnet.am>
2020-11-18 20:37:39 +0400

    defaultuser

--- sailfish.sh
+++ sailfish.sh
@@ -1,7 +1,7 @@
 #xa2
 sudo ifconfig usb0 192.168.2.12
 sshfs defaultuser@192.168.2.15:/home/defaultuser /mnt/nemo
-sshfs nemo@192.168.2.15:/run/media/nemo/ae47bf37-c663-4602-bc9e-ed455c441ab9 /mnt/nemo
+sshfs nemo@192.168.2.15:/run/media/defaultuser/ae47bf37-c663-4602-bc9e-ed455c441ab9 /mnt/nemo
 
 
 #jolla 1
