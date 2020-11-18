0f6f06dbff352895078b29ee9199900c50f93321
Norayr Chilingarian <norayr@arnet.am>
2020-11-18 20:35:31 +0400

    latest sailfish changes of user name

--- sailfish.sh
+++ sailfish.sh
@@ -1,6 +1,6 @@
 #xa2
 sudo ifconfig usb0 192.168.2.12
-sshfs nemo@192.168.2.15:/home/nemo /mnt/nemo
+sshfs defaultuser@192.168.2.15:/home/defaultuser /mnt/nemo
 sshfs nemo@192.168.2.15:/run/media/nemo/ae47bf37-c663-4602-bc9e-ed455c441ab9 /mnt/nemo
 
 
