11d3f33b16b849b026cecedd49480ca1549cf7e3
Norayr Chilingarian <norayr@arnet.am>
2021-01-16 23:43:02 +0400

    correct aspect ratio

--- scripts/video/canon_6d.sh
+++ scripts/video/canon_6d.sh
@@ -1,3 +1,9 @@
+#r modprobe v4l2loopback exclusive_caps=1 max_buffers=2
+#correct aspect ratio
+#r gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -s 1024x683 -threads 2 -f v4l2 /dev/video4
+#r gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -s 1368x912 -threads 2 -f v4l2 /dev/video4
+
+
 #hints from
 #https://medium.com/nerdery/dslr-webcam-setup-for-linux-9b6d1b79ae22
 #https://trac.ffmpeg.org/wiki/Capture/Webcam
