86bd1c5ce033ce09f80f976f0154362017d743bf
Norayr Chilingarian <norayr@arnet.am>
2021-08-26 15:56:11 +0400

    radios

---
+++ scripts/radio/dhr.sh
@@ -0,0 +1,3 @@
+#mplayer "https://deephouseradio.radioca.st/deep?type=http&nocache=107359"
+#mplayer "https://deephouseradio.radioca.st/deep?type=http"
+mplayer http://deephouseradio.radioca.st/deep
