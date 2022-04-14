ae73224b4263a7db45f0eb7404dd680c6f4eac24
Norayr Chilingarian <norayr@arnet.am>
2022-04-14 18:36:30 +0400

    added link which describes that.

--- scripts/pinebook_pro/detect_emmc.sh
+++ scripts/pinebook_pro/detect_emmc.sh
@@ -1,3 +1,4 @@
 #this is used when the pinebook booted from sdcard because emmc is disabled, to enable emmc
+#https://wiki.pine64.org/index.php?title=Pinebook_Pro#eMMC_information
 echo fe330000.mmc >/sys/bus/platform/drivers/sdhci-arasan/unbind
 echo fe330000.mmc >/sys/bus/platform/drivers/sdhci-arasan/bind
