7252929c7a0505a2dad4ced6f4b75c30d0022929
Norayr Chilingarian <norayr@arnet.am>
2022-05-07 00:54:52 +0400

    determining device to use with camera.

--- scripts/video/canon_6d.sh
+++ scripts/video/canon_6d.sh
@@ -1,7 +1,8 @@
 #r modprobe v4l2loopback exclusive_caps=1 max_buffers=2
+DEV=v4l2-ctl --list-devices | grep Dummy -1 | grep '/dev/'
 #correct aspect ratio
-#r gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -s 1024x683 -threads 2 -f v4l2 /dev/video4
-#r gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -s 1368x912 -threads 2 -f v4l2 /dev/video4
+#r gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -s 1024x683 -threads 2 -f v4l2 $DEV
+#r gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -s 1368x912 -threads 2 -f v4l2 $DEV
 
 
 #hints from
