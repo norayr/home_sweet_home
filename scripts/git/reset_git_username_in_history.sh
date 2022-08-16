c8e387b2184dc88a0e242882e895ab60b46ba914
Norayr Chilingarian <norayr@arnet.am>
2022-08-16 19:44:35 +0400

    reset git username in history, from here: https://gist.github.com/bgromov/a1905055a8b9cdbeb1d2a87e70920cc8

---
+++ scripts/git/reset_git_username_in_history.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Credits: http://stackoverflow.com/a/750191
+
+git filter-branch -f --env-filter "
+    GIT_AUTHOR_NAME='Newname'
+    GIT_AUTHOR_EMAIL='new@email'
+    GIT_COMMITTER_NAME='Newname'
+    GIT_COMMITTER_EMAIL='new@email'
+  " HEAD
