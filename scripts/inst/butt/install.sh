43cda7d5cc9dfe0bc3ab2051f69a47a86849356c
Norayr Chilingarian <norayr@arnet.am>
2023-07-09 02:47:55 +0400

    newer versions of fltk and butt

--- scripts/inst/butt/install.sh
+++ scripts/inst/butt/install.sh
@@ -1,7 +1,12 @@
 set -x
-FLTKVER=1.3.5
-BTVER=0.1.30
+#https://yer.dl.sourceforge.net/project/butt/butt/butt-0.1.37/butt-0.1.37.tar.gz
+#https://www.fltk.org/pub/fltk/1.3.8/fltk-1.3.8-source.tar.bz2
+FLTKVER=1.3.8
+BTVER=0.1.37
 BASEDIR=$(dirname "$0")
+
+wget -c https://yer.dl.sourceforge.net/project/butt/butt/butt-${BTVER}/butt-${BTVER}.tar.gz
+wget -c https://www.fltk.org/pub/fltk/${FLTKVER}/fltk-${FLTKVER}-source.tar.bz2
 
 cp $BASEDIR/fltk-${FLTKVER}-source.tar.bz2 /tmp/
 cd /tmp
@@ -16,5 +21,6 @@ cd /tmp
 tar zxvf butt-${BTVER}.tar.gz
 cd butt-${BTVER}
 LDFLAGS="-L/opt/radio/fltk-$FLTKVER/lib" ./configure --prefix=/opt/radio/butt-${BTVER}
+./configure --prefix=/opt/radio/butt-${BTVER}
 make
 sudo make install
