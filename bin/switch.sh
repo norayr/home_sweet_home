f05d6cef6ffd239d374031fe200da8c3a83a08cd
Norayr Chilingarian <norayr@arnet.am>
2023-09-22 19:13:44 +0400

    better esperanto.

--- bin/switch.sh
+++ bin/switch.sh
@@ -6,7 +6,8 @@ then
 setxkbmap "us(dvp), am, ru(phonetic_dvorak)" -option "grp:lalt_shift_toggle"
 elif [ "$1" == "eo" ]
 then
-	setxkbmap "us(dvp), am, epo(dvorak)" -option "grp:lalt_shift_toggle,lv3:ralt_switch" 
+	setxkbmap "us(dvp), am" -option "esperanto:dvorak,grp:lalt_shift_toggle,lv3:ralt_switch"
+	#setxkbmap "us(dvp), am, epo(dvorak)" -option "grp:lalt_shift_toggle,lv3:ralt_switch" 
 elif [ "$1" == "ge" ]
 then
   setxkbmap "us(dvp), am, ge" -option "grp:lalt_shift_toggle"
