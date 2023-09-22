eed99fcfb99f050c8932846a4881fc620002ced7
Norayr Chilingarian <norayr@arnet.am>
2023-09-22 14:58:02 +0400

    windows and tabs locations update.

--- bin/switch.sh
+++ bin/switch.sh
@@ -1,13 +1,15 @@
+#set -x
 if [ "$1" == "ru" ]
 then
-#setxkbmap "us(dvp), am, ru(phonetic)" -option "grp:alt_shift_toggle"
-#setxkbmap "us(dvp), am, ru" -option "grp:alt_shift_toggle"
-setxkbmap "us(dvp), am, ru(phonetic_dvorak)" -option "grp:alt_shift_toggle"
+#setxkbmap "us(dvp), am, ru(phonetic)" -option "grp:lalt_shift_toggle"
+#setxkbmap "us(dvp), am, ru" -option "grp:lalt_shift_toggle"
+setxkbmap "us(dvp), am, ru(phonetic_dvorak)" -option "grp:lalt_shift_toggle"
+elif [ "$1" == "eo" ]
+then
+	setxkbmap "us(dvp), am, epo(dvorak)" -option "grp:lalt_shift_toggle,lv3:ralt_switch" 
+elif [ "$1" == "ge" ]
+then
+  setxkbmap "us(dvp), am, ge" -option "grp:lalt_shift_toggle"
 else
-  if [ "$1" == "ge" ]
-	then
-	  setxkbmap "us(dvp), am, ge" -option "grp:alt_shift_toggle"
-	else
-    setxkbmap "us(dvp), am" -option "grp:alt_shift_toggle"
-	fi
+  setxkbmap "us(dvp), am" -option "grp:lalt_shift_toggle"
 fi
