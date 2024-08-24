133e748d7625230d0a2f6c81c1f3a4a8c0f0fa5d
Norayr Chilingarian <norayr@arnet.am>
2024-08-24 06:41:34 +0400

    new dhr link.

--- scripts/radio/dhr.sh
+++ scripts/radio/dhr.sh
@@ -1,3 +1,5 @@
 #mplayer "https://deephouseradio.radioca.st/deep?type=http&nocache=107359"
 #mplayer "https://deephouseradio.radioca.st/deep?type=http"
-mplayer http://deephouseradio.radioca.st/deep
+#mplayer http://deephouseradio.radioca.st/deep
+mplayer http://streaming.shoutcast.com/dhr
+
