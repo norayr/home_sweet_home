a35d9735e1e9541092fbe805185757fff90972d8
Norayr Chilingarian <norayr@arnet.am>
2024-10-09 16:42:48 +0400

    another example of x11grab.

--- scripts/video/grab_region_video.sh
+++ scripts/video/grab_region_video.sh
@@ -1,1 +1,2 @@
-ffmpeg -f x11grab -framerate 25 -video_size 100x130 -i :0.0+0,460 out2.mpg
+#ffmpeg -f x11grab -framerate 25 -video_size 100x130 -i :0.0+0,460 out2.mpg
+ffmpeg -f x11grab -framerate 25 -video_size 1280x800 -i :0.0+0,290 /tmp/out.mp4
