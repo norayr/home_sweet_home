b57549c64509236fff263ba8a0cf1d6332e7875b
Norayr Chilingarian <norayr@arnet.am>
2024-11-29 15:13:05 +0400

    alsa, need to test though.

--- scripts/video/grab_region_video.sh
+++ scripts/video/grab_region_video.sh
@@ -1,2 +1,3 @@
 #ffmpeg -f x11grab -framerate 25 -video_size 100x130 -i :0.0+0,460 out2.mpg
 ffmpeg -f x11grab -framerate 25 -video_size 1280x800 -i :0.0+0,290 /tmp/out.mp4
+#ffmpeg -f alsa -ac 2 -f x11grab -framerate 25 -video_size 1280x800 -i :0.0+0,290 /tmp/out.mp4
